f2a75aab7d35d1fdf2bfffcf531bea7371f329b2
Valeriu Dumitru Ovidiu <valeriuovidiu1999@gmail.com>
2023-10-06 16:33:41 +0300

    t message"mistake"
    "this commit wos a mistake"
    commit message
    Revert "search and podiums functionalities"
    
    This reverts commit 5c205ee8ea0cfa1ece506f9c896d040b44578412.

---
+++ Charts/urls.py
@@ -0,0 +1,21 @@
+from django.urls import path
+
+from  charts.views import  charts_data,journals
+from django.conf import settings   
+from django.conf.urls.static import static
+
+       
+urlpatterns = [     
+    path('get_data_for_work_chart/<str:username>/<str:searched_date>', charts_data.WorkedChartData.as_view(), name='get_data'),
+    path("worked_hours_save",charts_data.worked_hours_save,name="worked_hours_save"),    
+    path("search_by_date/<str:chart_name>", charts_data.search_by_date ,name="search_by_date"),  
+    path('get_data_for_sleep_chart/<str:username>/<str:searched_date>', charts_data.SleptChartData.as_view(), name='get_data_for_sleep_chart'),
+    path("slept_hours_save",charts_data.slept_hours_save,name="slept_hours_save"), 
+    path("journals_data/<str:username>/<str:searched_date>",charts_data.JournalsChartData.as_view(),name="jurnals_data"), 
+    path("upload_journal",journals.upload_journal,name="upload_jurnal"),
+    path("upload_calendar_image",journals.upload_calendar_image,name="upload_calendar_image"),
+    path("edit",journals.edit_journal,name="edit"),     
+]        
+
+if settings.DEBUG:   
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
